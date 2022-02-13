74d4997cce5567f6118844032cbe99ceb4ba885e
Imran Azam <imran.azam@and.digital>
2022-02-13 18:31:02 +0000

    updated App component to use GameEngine instance.

--- src/App.tsx
+++ src/App.tsx
@@ -8,22 +8,27 @@ import {
   TableContainer,
   TableRow,
 } from '@mui/material';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
-import { Cell, CellStatus } from './models/cell';
-import { color_palette } from './models/constants/constants';
-
-const rows = 10;
-const cols = 10;
+import { Cell } from './models/cell';
+import { color_palette } from './models/constants';
+import { GameEngine } from './services/game-engine';
 
 export const App = () => {
-  const [gridState, setGridState] = useState(createNewGrid());
+  const [gridState, setGridState] = useState([]);
+
+  const gameEngine = new GameEngine(gridState, setGridState);
+
+  // initialize game
+  useEffect(() => {
+    gameEngine.createNewGrid();
+  }, []);
 
   return (
     <Container maxWidth="md" className="App">
       <h1>Game of Life</h1>
       <div className="prettyGridTable">
-        {renderPrettyGridTable(gridState, setGridState)}
+        {renderPrettyGridTable(gridState, gameEngine)}
       </div>
       <div className="controls">
         <Button
@@ -35,7 +40,7 @@ export const App = () => {
             color: color_palette.terra_cotta,
             fontSize: '18px',
           }}
-          onClick={() => computeNextGen(gridState, setGridState)}
+          onClick={() => gameEngine.computeNextGeneration()}
         >
           Next gen
         </Button>
@@ -48,7 +53,7 @@ export const App = () => {
             fontSize: '18px',
             color: color_palette.terra_cotta,
           }}
-          onClick={() => setRandomGridState(gridState, setGridState)}
+          onClick={() => gameEngine.setRandomGridState()}
         >
           Random Gen
         </Button>
@@ -62,7 +67,7 @@ export const App = () => {
             color: color_palette.opal,
             fontSize: '18px',
           }}
-          onClick={() => setGridState(createNewGrid())}
+          onClick={() => gameEngine.createNewGrid()}
         >
           Reset
         </Button>
@@ -84,93 +89,7 @@ export const App = () => {
   );
 };
 
-const computeNextGen = (gridState: Cell[][], setGridState) => {
-  let ng = [];
-
-  for (let x = 0; x < rows; x++) {
-    ng[x] = [];
-    for (let y = 0; y < cols; y++) {
-      const cellStatus = gridState[x][y].status;
-      const neighboursCount = countCellNeighbours(gridState, x, y);
-
-      if (cellStatus === CellStatus.ALIVE) {
-        if (neighboursCount === 2 || neighboursCount === 3) {
-          ng[x][y] = CellStatus.ALIVE;
-        } else {
-          ng[x][y] = CellStatus.DEAD;
-        }
-      } else {
-        if (neighboursCount === 3) {
-          ng[x][y] = CellStatus.ALIVE;
-        } else {
-          ng[x][y] = CellStatus.DEAD;
-        }
-      }
-    }
-  }
-
-  let newGridState = [...gridState];
-  for (let x = 0; x < rows; x++) {
-    for (let y = 0; y < cols; y++) {
-      newGridState[x][y].status = ng[x][y];
-    }
-  }
-
-  setGridState(newGridState);
-};
-
-const countCellNeighbours = (gridState: Cell[][], cx: number, cy: number) => {
-  const neighbors = [
-    [-1, -1],
-    [-1, 0],
-    [-1, 1],
-    [0, -1],
-    [0, 1],
-    [1, -1],
-    [1, 0],
-    [1, 1],
-  ];
-  let count = 0;
-
-  neighbors.forEach(([x, y]) => {
-    const newI = cx + x;
-    const newJ = cy + y;
-
-    if (newI >= 0 && newI < rows && newJ >= 0 && newJ < cols) {
-      if (gridState[newI][newJ].status === CellStatus.ALIVE) count++;
-    }
-  });
-
-  return count;
-};
-
-const updateCellStatus = (
-  x: number,
-  y: number,
-  newCellState: CellStatus,
-  gridState: Cell[][],
-  setGridState
-) => {
-  let newGridState = [...gridState];
-  newGridState[x][y].status = newCellState;
-  setGridState(newGridState);
-};
-
-const toggleCellStatus = (
-  x: any,
-  y: any,
-  gridState: Cell[][],
-  setGridState
-) => {
-  const newStatus =
-    gridState[x][y].status === CellStatus.ALIVE
-      ? CellStatus.DEAD
-      : CellStatus.ALIVE;
-
-  updateCellStatus(x, y, newStatus, gridState, setGridState);
-};
-
-const renderPrettyGridTable = (gridState: Cell[][], setGridState) => {
+const renderPrettyGridTable = (gridState: Cell[][], gameEngine) => {
   return (
     <TableContainer component={Paper}>
       <Table size="small" aria-label="a dense table">
@@ -185,9 +104,7 @@ const renderPrettyGridTable = (gridState: Cell[][], setGridState) => {
                     padding="normal"
                     size="medium"
                     className="tableCell"
-                    onClick={() =>
-                      toggleCellStatus(cell.x, cell.y, gridState, setGridState)
-                    }
+                    onClick={() => gameEngine.toggleCellStatus(cell.x, cell.y)}
                     style={{
                       background: cell.status
                         ? color_palette.terra_cotta
@@ -202,43 +119,6 @@ const renderPrettyGridTable = (gridState: Cell[][], setGridState) => {
       </Table>
     </TableContainer>
   );
-};
-
-const createNewGrid = (): Cell[][] => {
-  let grid: Cell[][] = [];
-
-  for (let i = 0; i < rows; i++) {
-    grid[i] = [];
-    for (let j = 0; j < cols; j++) {
-      grid[i][j] = {
-        status: CellStatus.DEAD,
-        x: i,
-        y: j,
-      };
-    }
-  }
-
-  console.log('rset grid');
-  return grid;
-};
-
-const setRandomGridState = async (gridState, setGridState) => {
-  setGridState(createNewGrid());
-
-  let aliveCellsNum = 25;
-  let i = 0;
-
-  do {
-    const x = Math.floor(Math.random() * rows);
-    const y = Math.floor(Math.random() * rows);
-    const cellStatus = gridState[x][y].status;
-
-    if (cellStatus === CellStatus.DEAD) {
-      updateCellStatus(x, y, CellStatus.ALIVE, gridState, setGridState);
-    }
-
-    i++;
-  } while (i < aliveCellsNum);
 };
 
 export default App;
